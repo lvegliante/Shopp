a1010cc1fbfac896717998a693e877fb30b418e1
lvegliante <47487004+lvegliante@users.noreply.github.com>
2019-02-12 19:51:54 -0500

    Products Controller

---
+++ Shopp.UIClassic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Shopp.UIClassic
+{
+    public class Class1
+    {
+    }
+}
---
+++ Shopp.Web/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace Shopp.Web.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Shopp.Web.Data.Entities;
+
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+    }
+
+}
