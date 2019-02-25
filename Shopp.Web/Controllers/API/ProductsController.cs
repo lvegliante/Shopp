2f2d10cb5e54421e52dadc2bb3ae92133ddc53d4
lvegliante <47487004+lvegliante@users.noreply.github.com>
2019-02-24 20:13:54 -0500

    API Product controller video 13

---
+++ Shopp.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Shopp.Web.Controllers.API
+{
+    using Data;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[Controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(this.productRepository.GetAll());
+        }
+    }
+
+}
